cb06d981038a34c2c36d79b4678208871e1d5335
Ompilela Mulaudzi <ompiemulaudzi@gmail.com>
2025-08-31 01:50:44 +0200

    Add insert_test_event migration

---
+++ supabase/migrations/20250830170000_insert_test_event.sql
@@ -0,0 +1,8 @@
+INSERT INTO event (event_id, event_type, event_date, event_time, created_at)
+VALUES (
+    '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    'Test Event',
+    '2025-09-30',
+    '20:00:00',
+    '2025-08-30 16:00:00'
+);
